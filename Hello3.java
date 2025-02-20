7c69c74bdde255e961048ade7bc42a0322b975a9
Sumaj25mhj <s4144999@student.rmit.edu.au>
2025-02-20 14:55:03 +1100

    Add files via upload

---
+++ Hello3.java
@@ -0,0 +1,21 @@
+public class Hello3 {
+
+    public static void main(String[] args) {
+        double sum;
+        double diff;
+        double prod;
+        double quot;
+        int num1 = 233;
+        int num2 = 11;
+        sum = num1 + num2; 
+        diff = num1 - num2;
+        prod = num1 * num2; 
+        quot = (double)num1 / num2;
+        System.out.println(sum);
+        System.out.println("Diff of " + num1 + " and " + num2 + " is " + diff);
+        System.out.println("Prod of " + num1 + " and " + num2 + " is " + prod);
+        System.out.println("Quot of " + num1 + " and " + num2 + " is " + quot);
+
+
+    }
+}
